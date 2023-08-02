230fe41f32f310cf1d254fc6ad1cf61255b97edb
MinhPhap1508 <minhphapclt.email@gmail.com>
2023-08-02 17:31:07 +0700

    bai TAp ss13

--- ss6/bai_tap/app_blog/src/main/java/com/example/app_blog/controller/RestBlogController.java
+++ ss6/bai_tap/app_blog/src/main/java/com/example/app_blog/controller/RestBlogController.java
@@ -3,11 +3,15 @@ package com.example.app_blog.controller;
 import com.example.app_blog.model.Blog;
 import com.example.app_blog.service.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @CrossOrigin("*")
@@ -16,13 +20,24 @@ public class RestBlogController {
     @Autowired
     private IBlogService blogService;
 
+//        @GetMapping("")
+//    public ResponseEntity<List<Blog>> getList() {
+//        List<Blog> blogList = blogService.showList();
+//        if ((blogList.isEmpty())) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        }
+//        return new ResponseEntity<>(blogList, HttpStatus.OK);
+//    }
+
     @GetMapping("")
-    public ResponseEntity<List<Blog>> getList() {
-        List<Blog> blogList = blogService.showList();
-        if ((blogList.isEmpty())) {
+    public ResponseEntity<Page<Blog>> getList(@RequestParam(defaultValue = "0") int page,
+                                              @RequestParam(defaultValue = "") String searchName) {
+        Pageable pageable = PageRequest.of(page, 2, Sort.by("name").descending());
+        Page<Blog> blogPage = blogService.findAll(pageable,searchName);
+        if(blogPage.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<>(blogList, HttpStatus.OK);
+        return new ResponseEntity<>(blogPage,HttpStatus.OK);
     }
 
     @PostMapping
@@ -54,7 +69,7 @@ public class RestBlogController {
     @GetMapping("/{id}")
     public ResponseEntity<Blog> detail(@PathVariable int id) {
         Blog blog = blogService.getById(id);
-        if(blog==null){
+        if (blog == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(blog, HttpStatus.OK);
